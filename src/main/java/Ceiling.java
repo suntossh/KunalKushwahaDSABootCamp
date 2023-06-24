b558578d6c7926080d1e9c1f3ace41f0d9a6b9f0
Santoshkumar pandey <suntossh@users.noreply.github.com>
2023-06-23 23:12:40 -0400

    Order Agnostic Binary Search in Asc and desc Sorted Array

---
+++ src/main/java/Ceiling.java
@@ -0,0 +1,30 @@
+public class Ceiling {
+
+    public static void main(String[] args) {
+        System.out.println("Hari Bol");
+        int[] array = {2, 3, 5, 9, 14, 16, 18};
+        int ceiling = findCeiling(array, 15);
+        System.out.println("ceiling, index of smallest number greater than equal to target = "+ceiling);
+
+
+    }
+
+    public static int findCeiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+
+            int midIndex = start + ((end - start) / 2);
+            System.out.println("Middle Index Value = " + midIndex);
+            if ( target < arr[midIndex]) {
+                end = midIndex - 1;
+            } else if (target > arr[midIndex]) {
+                start = midIndex + 1;
+            } else {
+                return midIndex;
+            }
+        }
+        return start;
+    }
+}
---
+++ src/main/java/Floor.java
@@ -0,0 +1,30 @@
+public class Floor {
+
+    public static void main(String[] args) {
+        System.out.println("Hari Bol");
+        int[] array = {2, 3, 5, 9, 14, 16, 18};
+        int floor = findFloor(array, 15);
+        System.out.println("Floor, index of biggest number less than equal to target = "+floor);
+
+
+    }
+
+    public static int findFloor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+
+            int midIndex = start + ((end - start) / 2);
+            System.out.println("Middle Index Value = " + midIndex);
+            if ( target < arr[midIndex]) {
+                end = midIndex - 1;
+            } else if (target > arr[midIndex]) {
+                start = midIndex + 1;
+            } else {
+                return midIndex;
+            }
+        }
+        return end;
+    }
+}
