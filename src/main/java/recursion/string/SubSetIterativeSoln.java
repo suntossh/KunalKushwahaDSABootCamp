dccf19a4071fdd153952c77b34baafc72c0beb72
Santoshkumar pandey <suntossh@users.noreply.github.com>
2024-12-29 16:41:32 -0500

    Subsequence of Strings Creating Subsets of a String Creating Subset of an array iteratively and handling Duplicates.

--- src/main/java/recursion/string/SubSetIterativeSoln.java
+++ src/main/java/recursion/string/SubSetIterativeSoln.java
@@ -1,6 +1,7 @@
 package recursion.string;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSetIterativeSoln {
@@ -10,9 +11,11 @@ public class SubSetIterativeSoln {
 
 
     public static void main(String[] args) {
-        String[] arr = {"1", "2", "3"};
-        List<List<String>> subseqences = subSet(arr);
+        //String[] arr = {"1", "2", "3"};
+        String[] arr = {"1", "2", "2"};
+        List<List<String>> subseqences = subSetDuplicateII(arr);
         System.out.println(subseqences);
+
 
     }
 
@@ -25,6 +28,52 @@ public class SubSetIterativeSoln {
             for (int i = 0; i < n; i++) {
                 List<String> tempList = new ArrayList<String>(outer.get(i));
                 tempList.add(str);
+                outer.add(tempList);
+            }
+        }
+        return outer;
+    }
+
+    private static List<List<String>> subSetDuplicate(String[] arr) {
+        Arrays.sort(arr);
+        List<List<String>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        String prev = null;
+        String curr = null;
+        for (String str : arr) {
+            int n = outer.size();
+            int index = 0;
+            if (str.equalsIgnoreCase(prev)) {
+                index = (outer.size() / 2);
+                n = (outer.size() / 2);
+            }
+            prev = str;
+            for (int i = 0; i < n; i++) {
+                List<String> tempList = new ArrayList<String>(outer.get(i + index));
+                tempList.add(str);
+                outer.add(tempList);
+            }
+        }
+        return outer;
+    }
+
+    private static List<List<String>> subSetDuplicateII(String[] arr) {
+        Arrays.sort(arr);
+        List<List<String>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        int start = 0;
+        int end = 0;
+
+        for (int j = 0; j < arr.length; j++) {
+            if(j>0 && arr[j] == arr[j-1]){
+                start = end + 1;
+            }
+            end = outer.size() -1;
+            int n = outer.size();
+            for (int i = start; i < n; i++) {
+                List<String> tempList = new ArrayList<String>(outer.get(i));
+                tempList.add(arr[j]);
                 outer.add(tempList);
             }
         }
